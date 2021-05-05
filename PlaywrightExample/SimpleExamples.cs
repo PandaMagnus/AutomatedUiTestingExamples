efba281adaf18fb844c70839318d608e844e7865
Mike Curn <mjcurn@gmail.com>
2021-05-05 13:56:44 -0700

    Add scenario requiring multiple clicks

--- PlaywrightExample/SimpleExamples.cs
+++ PlaywrightExample/SimpleExamples.cs
@@ -15,5 +15,21 @@ namespace PlaywrightExample
             await page.GoToAsync("http://the-internet.herokuapp.com/");
             Assert.True(await page.IsVisibleAsync("css=h1[class='heading']"));
         }
+
+        [Fact]
+        public async Task ClickScenario()
+        {
+            using var pw = await Playwright.CreateAsync();
+            await using var browser = await pw.Chromium.LaunchAsync(headless: false);
+            var page = await browser.NewPageAsync();
+            await page.GoToAsync("https://the-internet.herokuapp.com/tinymce");
+
+            await page.ClickAsync("//span[text() = 'Format']");
+            await page.ClickAsync("//div[text() = 'Formats']");
+            await page.ClickAsync("//div[text() = 'Headings']");
+            await page.ClickAsync("//h1[text() = 'Heading 1']");
+
+            Assert.Equal("Heading 1", await page.GetTextContentAsync("span[class='tox-tbtn__select-label']"));
+        }
     }
 }
